a4594ff982c7aa01db5c868fbd304dc40faea069
Pig Fang <g-plane@hotmail.com>
2018-12-14 17:08:52 +0800

    beautify

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
-$username = "londist";
-$password = "2012";
+$username = "root";
+$password = "root";
 $host = "localhost";
 
-$db_name = "php";
+$db_name = "canteen";
 
 $db = mysql_connect($host, $username,$password) or die("连接数据库失败".mysql_error());
 mysql_select_db($db_name,$db);
--- consumption_record/add_eating.php
+++ consumption_record/add_eating.php
@@ -1,39 +1,51 @@
-﻿<?php
-header("Content-type: text/html; charset=utf-8");
-?>
-<html>
-    <head>
-        <title>模拟就餐刷卡</title>
-        <style type="text/css" media="screen">
-            body{margin:0px; padding:0px; background:#f3f3f3; text-align:center;}
-            form {width:600px; background:white; margin:0 auto; margin-top:30px; padding:10px; text-align:center;}
-            form {-moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius:10px; }
-            td {border-bottom:1px dashed #ccc; padding:5px 25px; text-align:center;}
+<?php
+include ("../conn.php");
+include ("../util.php");
+utf8();
 
-        </style>
-    </head>
-    <body>
-        <form action="add_consumption_record.php" method="post">
-            <h3> 模拟就餐刷卡 </h3>
-            <p>学号<input type="text" name="id" size=20><input type="submit" value="点菜!"> <input type="reset" value="清空"></p>
-            <table >
-                <tr><th> 序号 </th><th> 菜名 </th><th> 价格(元) </th><th> 描述 </th><th> 选择 </th></tr>
-            <?php
-                    include ("../conn.php");
-                    include ("../util.php");
-                    utf8();
-                    $sql = "select * from food";
-                    $result = mysql_query($sql,$db);
-                    if (! isset($result))
-                        die("查询食物失败! <br/>".mysql_error());
-                    $n = 1;
-                    while ($row = mysql_fetch_array($result)){
-                        printf("<tr><td>%d</td><td>%s</td><td>%s</td><td>%s</td>",$n,$row['name'],$row['price'],$row['description']);
-                        printf("<td><input type='checkbox' name='food[]' value='%s'></td></tr>",$row['fid']);
-                        $n = $n + 1;
-                    }
-                    mysql_close($db);
-                    ?>
-        </form>
-    <body>
-</html>
+$sql = "select * from food";
+$result = mysql_query($sql,$db);
+if (! isset($result))
+    die("查询食物失败<br/>".mysql_error());
+include ("../template/template.html");
+?>
+<section class="content-header"><h1>消费记录</h1></section>
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header">模拟就餐刷卡</div>
+                <form action="add_consumption_record.php" method="post">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="id">学号</label>
+                            <input type="text" name="id" class="form-control">
+                        </div>
+                        <table class="table table-hover">
+                            <tr>
+                                <th>序号</th>
+                                <th>菜名</th>
+                                <th>价格</th>
+                                <th>描述</th>
+                                <th>选择</th>
+                            </tr>
+<?php
+$n = 1;
+while ($row = mysql_fetch_array($result)){
+    echo "<tr><td>$n</td><td>".$row['name']."</td><td>".$row['price']."</td><td>".$row['description']."</td><td><input type='checkbox' name='food[]' value='".$row['fid']."'></td></tr>";
+    $n = $n + 1;
+}
+mysql_close($db);
+?>
+                        </table>
+                    </div>
+                    <div class="box-footer">
+                        <input class="btn btn-primary" type="submit" value="点菜">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include ("../template/tail.html");
